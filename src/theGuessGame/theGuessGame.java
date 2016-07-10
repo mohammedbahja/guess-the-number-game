d2181ced3e97d3007454769adeb76b1f001db452
Mohammed Bahja <mohammed.bahja@brunel.ac.uk>
2016-07-10 23:47:47 +0100

    get ready for the player to use the keyboard
    guess-the-number-game/issues#1

--- src/theGuessGame/theGuessGame.java
+++ src/theGuessGame/theGuessGame.java
@@ -4,7 +4,11 @@ import java.util.Scanner;
 public class theGuessGame {
 	public static void main(String[] args) {
 		
-		
+		// get ready for the player to use the keyboard
+				Scanner scan = new Scanner(System.in);
+				
+				// play again string
+				String playAgain = "";
 	}
 
 }
