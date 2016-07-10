58e5eb53c5e0d00ca87d4351760dde42c6471113
Mohammed Bahja <mohammed.bahja@brunel.ac.uk>
2016-07-10 23:50:20 +0100

    while loop for guessing
    guess-the-number-game/issues#1

--- src/theGuessGame/theGuessGame.java
+++ src/theGuessGame/theGuessGame.java
@@ -11,6 +11,24 @@ public class theGuessGame {
 				String playAgain = "";
 				// create a random number from 1-100
 				int theNumber = (int)(Math.random()*100 + 1);
+				while ( guess != theNumber ) {
+					System.out.println("Guess a number between 1 and 100:");
+					
+					// get the user's guess
+					guess = scan.nextInt();
+					
+					// add one to the number of tries
+					 
+					
+					if (guess < theNumber)
+						System.out.println(guess + " is too low. Try again.");
+					else if (guess > theNumber)
+						System.out.println(guess + " is too high. Try again.");
+					else
+						System.out.println(guess + " is correct! You win!" );
+					
+				}	// end of while loop for guessing
+				
 	}
 
 }
